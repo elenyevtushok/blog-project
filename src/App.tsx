ccebbe8c0674d3a2d05449f180c7df54469d7bd8
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-04-13 10:54:33 +0200

    implemented useQuery for uploading comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import './App.css'
+
 import { Footer } from './pages/Footer'
 import { MainContent } from './pages/MainContent'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { HeaderHero } from './pages/HeaderHero'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
-
 	return (
 		<>
 			<Provider store={store}>
-				<HeaderHero />
-				<MainContent />
-				<Footer />
+				<QueryClientProvider client={queryClient}>
+					<HeaderHero />
+					<MainContent />
+					<Footer />
+				</QueryClientProvider>
 			</Provider>
 		</>
 	)
 }
 
 export default App
+
+
+
+
+
