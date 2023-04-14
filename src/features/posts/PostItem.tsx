123a756648fdb7bfce08bd84a5186f37c2a517e0
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-04-14 11:32:49 +0200

    delete unused imports

--- src/features/posts/PostItem.tsx
+++ src/features/posts/PostItem.tsx
@@ -3,11 +3,8 @@ import { Post } from './dto/Post'
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { deletePost } from './postsSlice';
 import { selectUserById } from '../users/usersSlice';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { PostEditModal } from './PostEditModal';
-import { selectComments, upsertComments } from '../comments/commentsSlice';
-import { getCommentsApi } from '../../api/comments-api';
-import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 
 
@@ -20,20 +17,6 @@ export const PostItem = ({ post }: { post: Post }) => {
 	const dispatch = useAppDispatch();
 	const user = useAppSelector(state => selectUserById(state, post.userId));
 
-	// useQuery({
-	// 	queryKey: ['comments', post.id],
-	// 	queryFn: () => getCommentsApi(post.id),
-	// 	onSuccess: ((comments) => {
-	// 		dispatch(upsertComments(comments))
-	// 	})
-	// })
-
-
-	// const comments = useAppSelector(selectComments);
-	// const postComments = useMemo(
-	// 	() => comments.filter((comment) => comment.postId === post.id),
-	// 	[comments, post.id]
-	// );
 
 	return (
 		<div className="post-in">
@@ -41,7 +24,6 @@ export const PostItem = ({ post }: { post: Post }) => {
 			<div className="content-seperator"></div>
 			<p className='post-author' data-testid={`post-author-${post.id}`}>Author: {user?.name}</p>
 			<p>{post.body}</p>
-			{/* <ul>{postComments?.map(comment => <li key={comment.id}>{comment.body}</li>)}</ul> */}
 			<div>
 				<button onClick={() => dispatch(deletePost(post))} className="delete-post-button" data-testid={`post-delete-button-${post.id}`}>
 					<DeleteOutlined />
